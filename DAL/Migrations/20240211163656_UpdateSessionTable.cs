902f4a2c9e0a048f2d8d85e951aae3a44801d6a4
Koichi-hub <gogotimur2020@gmail.com>
2024-02-11 21:52:27 +0500

    Control amount of all weather requests | tested success

---
+++ DAL/Migrations/20240211163656_UpdateSessionTable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    public partial class UpdateSessionTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateLastWeatherRequest",
+                table: "Sessions",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Sessions",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateLastWeatherRequest",
+                table: "Sessions");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Sessions");
+        }
+    }
+}
